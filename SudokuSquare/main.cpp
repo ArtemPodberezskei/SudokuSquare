24ddb064d2a03f31b3c6310f154e37993da4e15c
mac <artem.podberezskei@yandex.ru>
2022-11-11 15:53:54 +0300

    Initial Commit

---
+++ SudokuSquare/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SudokuSquare
+//
+//  Created by mac on 11.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
